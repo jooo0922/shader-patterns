29046810e648807ddff026f0bf09ca92955828ba
jooo0922 <2014102458@khu.ac.kr>
2021-11-01 01:21:22 +0900

    feat: Draw grad pattern

--- patterns.glsl
+++ patterns.glsl
@@ -16,11 +16,8 @@ void main() {
   // coord = mod(coord, 1.);
   coord = fract(coord);
 
-  // 반지름이 0.3을 step() 내장함수로 그린 것
-  // 즉, 각 픽셀들 좌표 ~ vec2(.5), 즉 원점 사이의 거리값을 잰 뒤, 0.3보다 크면 1, 작으면 0을 리턴해서 vec3로 만듦.
-  // 그니까 원점에서 0.3 반경 내에 있으면 black, 바깥에 있으면 white가 칠해지겠지
-  // 이제 이 원을 패턴화시켜줄 것임.
-  vec3 col = vec3(step(.3, distance(coord, vec2(.5))));
+  // 이번에는 각 픽셀들 좌표값을 이용해서 r, g 성분값을 지정함으로써, 정사각형 그라데이션을 그리고, 그것을 패턴화시킴.
+  vec3 col = vec3(coord, 1.);
 
   gl_FragColor = vec4(col, 1.);
 }
